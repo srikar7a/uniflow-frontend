c5fd6b86fc705901e83c3a4d0e6e39679b9ea3d9
Andrew Dixon <andrewdixon139@gmail.com>
2020-03-29 12:53:15 -0500

    Updates

---
+++ precache-manifest.c59d4c4bc2d3899154f3dc24191b121c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bd27318374d7eddfe67269e9bf17174",
+    "url": "/uniflow-frontend/index.html"
+  },
+  {
+    "revision": "1d617cde3efb6f4cc468",
+    "url": "/uniflow-frontend/static/css/main.30780317.chunk.css"
+  },
+  {
+    "revision": "d6ec138715dfa765feb8",
+    "url": "/uniflow-frontend/static/js/2.27fd7884.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/uniflow-frontend/static/js/2.27fd7884.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d617cde3efb6f4cc468",
+    "url": "/uniflow-frontend/static/js/main.92e480f7.chunk.js"
+  },
+  {
+    "revision": "21ca986db1f8c2f0cb47",
+    "url": "/uniflow-frontend/static/js/runtime-main.59cafa28.js"
+  },
+  {
+    "revision": "6e1267d9d946b0236cdf6ffd02890894",
+    "url": "/uniflow-frontend/static/media/favicon.6e1267d9.ico"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/uniflow-frontend/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fa1ded1ed7c11438a9b0385b1e112850",
+    "url": "/uniflow-frontend/static/media/robots.fa1ded1e.txt"
+  }
+]);
